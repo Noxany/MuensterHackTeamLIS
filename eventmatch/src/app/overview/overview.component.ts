529dd19a77abe076ee12aa2d73f39e38984af93c
xcsluca <lucavonderheiden@gmx.de>
2023-09-22 21:30:10 +0200

    first try card overview

--- eventmatch/src/app/overview/overview.component.ts
+++ eventmatch/src/app/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SpecificCardComponent } from '../specific-card/specific-card.component';
 
 @Component({
   selector: 'app-overview',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent {
-
+  public cardData : Array<SpecificCardComponent> = [{eventName: "Münsterhack" , eventHolder : "items GmbH", eventLocation : "Münster", eventContent: "This is some content", eventActions : "Nothing"},
+  {eventName: "Münsterhack" , eventHolder : "items GmbH", eventLocation : "Münster", eventContent: "This is some content", eventActions : "Nothing"},
+  {eventName: "Münsterhack" , eventHolder : "items GmbH", eventLocation : "Münster", eventContent: "This is some content", eventActions : "Nothing"},
+  {eventName: "Münsterhack" , eventHolder : "items GmbH", eventLocation : "Münster", eventContent: "This is some content", eventActions : "Nothing"}];
 }
